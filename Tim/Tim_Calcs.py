54a397ba8f11af70d472e3f0a4113bc9cc8f79f7
Akemi Takeuchi <takeuchiimeka@gmail.com>
2023-11-02 01:42:36 -0400

    screwed up the cals

--- Tim/Tim_Calcs.py
+++ Tim/Tim_Calcs.py
@@ -1,15 +1,27 @@
+#The purpose of this calculator is to eventually be able to simulate the system, being able to take any input and calculate
+#all information that could be useful while making variables accessible
+#but im overthinking everything in this so things might not make sense as i play around with stuff
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 import time
 
+#Length Values
 L_tether = 1000 #Length of tether (m)
 D_tether = 0.0007 #Diameter of tether (m)
 Dmin_spool = 0.03 #Smallest diameter of spool (m)
 Dmax_spool = 0.1 #Largest diameter of spool (m)
 L_sp = 0.1 #Length of spool (m)
-num_rot_spool = L_sp//D_tether
-print(num_rot_spool)
+num_rot_spool = L_sp//D_tether #142
+
+#Mass Values
+m_string = 0.0008889 #kg/m
+m_moby = 1.5 #kg
+m_spool = 
+g = 9.81 #m/s^2
+F_drag = 33.1 #N (assumption from doc, maybe will recalculate with variable parameters)
+percent_drag = 1 #what percent of drag are we using for the calc
+drag = dpercent_drag*F_drag
 
 ## == LENGTH CALCULATIONS == ##
 dia = np.arange(Dmin_spool + D_tether, 2*D_tether*1000 + Dmin_spool+D_tether + 2*D_tether, 2*D_tether) #the set of all diameters until 1000 layers
@@ -25,27 +37,21 @@ def distance(point_on_tether):
             layer = i
             wraps = leftover//(dia[i]*math.pi)
             angle = ((leftover/(dia[i]*math.pi))%1)*2*math.pi #radians
-            return layer, wraps, angle
+            return dia[i], layer, wraps, angle
             break
         elif leftover > layer_length:
             leftover = leftover - layer_length
             print("This much tether remaining: " + str(leftover))
 
+def torque(point_on_tether):
+    r = distance(point_on_tether)[0]/2
+    
+
 def timing(spool_rpm, point_on_tether):
     pass
+
 print(distance(1000))
 
-
-#print("The number layers on the spool is: " + str(layers))
-#print("Total number of wraps is: " + str(tot_wraps))
-'''
-plt.figure(1)
-plt.title('Distance Visualization')
-plt.plot(dist_tether, spool)
-plt.xlabel('Distance along tether')
-plt.ylabel('Position on spool')
-plt.show()
-'''
 ## == TORQUE AND RPM CALCS == ##
 
 class Motor:
